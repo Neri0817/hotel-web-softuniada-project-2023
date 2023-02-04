412ddc12a561de73e95b8b4000cbec81838d9b90
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2023-02-04 21:26:33 +0200

    Add files via upload

---
+++ src/assets/preloader.js
@@ -0,0 +1,11 @@
+let preloader = document.getElementById('preloader')
+
+window.addEventListener('load', function(){
+
+   this.window.setTimeout(function preload() {
+    
+    preloader.style.display = 'none'
+   }, 6000)
+
+   
+})
