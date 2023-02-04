928a7fb9acb61a0bb0df365f2f4b9efae85b7eb5
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-04 11:51:24 +0100

    initial server setup

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const viewEngineSetup = require('./config/viewEngine');
+const routes = require('./config/routes');
+const config = require('./config/config');
+const intitDB = require('./config/database');
+const initDB = require('./config/database');
+
+const app = express();
+viewEngineSetup(app);
+
+app.use(express.static('src/static'));
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(routes);
+
+initDB()
+    .then(() => app.listen(config.PORT, () => console.log(`Server is running on PORT: ${config.PORT}...`)))
+    .catch((err) => console.log(err));
+
