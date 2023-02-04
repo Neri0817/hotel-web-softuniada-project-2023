dc071bc2d6c01082424433156204e5d518c425d1
Neri <91215462+Neri0817@users.noreply.github.com>
2023-02-04 21:46:06 +0200

    Some changes

--- src/assets/preloader.js
+++ src/assets/preloader.js
@@ -1,11 +1,7 @@
-let preloader = document.getElementById('preloader')
+let preloader = document.getElementById("preloader");
 
-window.addEventListener('load', function(){
-
-   this.window.setTimeout(function preload() {
-    
-    preloader.style.display = 'none'
-   }, 6000)
-
-   
-})
+window.addEventListener("load", function () {
+  this.window.setTimeout(function preload() {
+    preloader.style.display = "none";
+  }, 3000);
+});
