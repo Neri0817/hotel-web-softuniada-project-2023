e871c250b78e2db8008c6d0c65aed5cf779b715e
Neri <91215462+Neri0817@users.noreply.github.com>
2023-02-06 22:58:43 +0200

    Update reservation-hotel page
    Add reservation confirming

---
+++ src/assets/checkboxInputs.js
@@ -0,0 +1,7 @@
+// this code runs in reserve-hotel.html
+function onlyOne(checkbox) {
+  var checkboxes = document.getElementsByName("check");
+  checkboxes.forEach((item) => {
+    if (item !== checkbox) item.checked = false;
+  });
+}
