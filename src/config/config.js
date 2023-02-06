d194ffe105b048cb8ca9c65d58f862da3e4a7606
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-06 14:05:11 +0100

    added SECRET for express-session

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,13 @@
 const config = {
     production: {
         PORT: 2222,
-        DB_URI: 'mongodb://localhost:27017/hotel'
+        DB_URI: 'mongodb://localhost:27017/hotel',
+        SECRET: 'F.O.U.R.secret'
     },
     development: {
         PORT: 3000,
-        DB_URI: 'mongodb://localhost:27017/hotelApp'
+        DB_URI: 'mongodb://localhost:27017/hotelApp',
+        SECRET: 'F.O.U.R.secret'
     }
 }
 
