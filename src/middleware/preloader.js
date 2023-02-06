a65f0401a8652355de160a6fcfafcdafe6efcfb8
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-06 13:09:22 +0100

    added scripts for preloader and slideshow in the mai.hbs

--- src/middleware/preloader.js
+++
@@ -1,7 +0,0 @@
-let preloader = document.getElementById("preloader");
-
-window.addEventListener("load", function () {
-  this.window.setTimeout(function preload() {
-    preloader.style.display = "none";
-  }, 3000);
-});
