e653fe41455d9c9cf7559204cfdd0120a600991d
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-06 14:11:03 +0100

    added .validateUser to check if password matches

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,10 +1,25 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
-exports.register = async(username, email, password) => {
+exports.register = async (username, email, password) => {
     const hash = await bcrypt.hash(password, 10);
-    
+
     return await User.create({ username, email, password: hash });
 };
 
-exports.checkUserExistence = (username) => User.findOne({username});
+exports.checkUserExistence = (username) => User.findOne({ username });
+
+exports.validateUser = async (username, password) => {
+    const user = await User.findOne({ username });
+    if (!user) return null;
+
+    console.log(`user: ${user}`)
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    
+    console.log(`isValid: ${isPasswordValid}`)
+    
+    if (!isValid) return null;
+
+    return user;
+};
