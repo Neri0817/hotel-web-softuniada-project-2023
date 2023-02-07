193fc4de023cd7bd0e5d7906f055c7608c5c9114
Neri <91215462+Neri0817@users.noreply.github.com>
2023-02-07 16:29:12 +0200

    Update reservation page
    add booking overwie

--- src/assets/checkboxInputs.js
+++ src/assets/checkboxInputs.js
@@ -1,7 +1,8 @@
-// this code runs in reserve-hotel.html
 function onlyOne(checkbox) {
   var checkboxes = document.getElementsByName("check");
   checkboxes.forEach((item) => {
     if (item !== checkbox) item.checked = false;
   });
 }
+
+// this code runs in reserve-hotel.html checkboxs
