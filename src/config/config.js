ac93255ba3e2152dd850107616c12f776972b363
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-08 12:29:02 +0100

    added ONE_DAY expiration

--- src/config/config.js
+++ src/config/config.js
@@ -2,12 +2,14 @@ const config = {
     production: {
         PORT: 2222,
         DB_URI: 'mongodb://localhost:27017/hotel',
-        SECRET: 'F.O.U.R.secret'
+        SECRET: 'F.O.U.R.secret',
+        ONE_DAY: 1000 * 60 * 60 * 24
     },
     development: {
         PORT: 3000,
         DB_URI: 'mongodb://localhost:27017/hotelApp',
-        SECRET: 'F.O.U.R.secret'
+        SECRET: 'F.O.U.R.secret',
+        ONE_DAY: 1000 * 60 * 60 * 24
     }
 }
 
