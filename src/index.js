0bd303cf8cde27a5d88ff1dbd28a6f58c6d74b8b
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-08 11:40:26 +0100

    refactoring session

--- src/index.js
+++ src/index.js
@@ -4,19 +4,29 @@ const routes = require('./config/routes');
 const config = require('./config/config');
 const initDB = require('./config/database');
 const session = require('express-session');
+const cookieParser = require("cookie-parser");
 
 const app = express();
 viewEngineSetup(app);
+
+//session middleware
+// creating 24 hours from milliseconds
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(session({
+    secret: config.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay }
+}));
 
 app.use(express.static('src/static'));
 app.use(express.urlencoded({
     extended: false
 }));
-app.use(session({
-    secret: config.SECRET,
-    resave: false,
-    saveUninitialized: true
-}));
+
+//cookie-parser middleware
+app.use(cookieParser());
+
 app.use(routes);
 
 initDB()
