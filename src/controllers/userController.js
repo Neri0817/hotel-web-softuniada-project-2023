4be791744fe43fb589588638a9acd1b839c593c8
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-02-11 08:53:32 +0200

    Added error message to pop up in join

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -23,6 +23,7 @@ exports.postLogin = async (req, res) => {
         console.log(req.session);
         res.redirect('/');
     } catch (error) {
+       
         console.log(`Error: ${error}`)
         //not sure if we need to destroy the session
         //req.session.destroy();
@@ -36,9 +37,12 @@ exports.getRegister = (req, res) => {
 exports.postRegister = async (req, res) => {
     const { username, email, password, repeatPassword } = req.body;
 
+    console.log(req.body);
+
     if (password !== repeatPassword) {
         //TODO: Error handling
-        return res.redirect('/404')
+        // return res.redirect('/404')
+        res.render('join', {error: 'Password mismatch!'});
     }
 
     //TO DO: think of validator function to validate the data in each field
@@ -47,13 +51,16 @@ exports.postRegister = async (req, res) => {
 
     if (existingUser) {
         //TODO: Error handling
-        return res.redirect('/404')
+        // return res.redirect('/404')
+        res.render('join', {error: 'User already exists'});
     }
 
     try {
         await userService.register(username, email, password);
     } catch (error) {
         console.log(`Error trying to POST register: ${error}`);
+        return res.render('join', {error: error.message});
+
     }
     res.redirect('/users/login');
 };
