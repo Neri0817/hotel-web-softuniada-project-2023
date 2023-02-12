60c39a3054e734b8dbe3f52a3154d3d49582fe55
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2023-02-12 16:48:04 +0200

    Update errorUtils.js

--- src/utils/errorUtils.js
+++ src/utils/errorUtils.js
@@ -22,3 +22,21 @@ exports.getErrorMessage = (error) => {
     }
 
 }
+
+exports.mapErrors = (err) => {
+    if (Array.isArray(err)) {
+        return err
+    }
+    else if(err.name == 'ValidationError'){
+        return Object.values(err.errors).map(e => ({msg: e.message}))
+    }else if(typeof err.message == 'string'){
+
+
+        
+        return [{msg:err.message}]
+    }else{
+        return [{msg:'Request error'}]
+    }
+
+
+}
